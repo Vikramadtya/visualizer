9d54b3e778fe96705d826d6826b1d06cb4e01a42
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2024-11-29 20:32:52 +0530

    minor fix

--- src/components/organism/blogs/atoms/blogCard.jsx
+++ src/components/organism/blogs/atoms/blogCard.jsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import { classNameResolver } from "@/utils/classNameResolver";
 
 const BlogCard = ({ title, description, slug, color }) => {
   return (
     <>
       <Link
         href={`/${slug}`}
-        className={`${color} block max-w-sm rounded-lg border border-gray-200 p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}
+        className="block max-w-sm rounded-lg border border-gray-200 p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        style={{ background: color }}
       >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
