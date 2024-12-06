89e18840cb8f8b32c89b1ad2249a9a7d75c4df90
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2024-12-06 10:41:33 +0530

    fix styling

--- src/components/organism/blogs/atoms/blogCard.jsx
+++ src/components/organism/blogs/atoms/blogCard.jsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
+import Pigeon from "../../../../../public/icons/walking.gif";
+import Image from "next/image";
 
 const BlogCard = ({ title, description, slug, color }) => {
   return (
     <>
       <Link
         href={`/${slug}`}
-        className="block max-w-sm rounded-lg border border-gray-200 p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-        style={{ background: color }}
+        className={`${color} block max-w-sm rounded-lg border border-gray-200 p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}
       >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h5>
+        <div className="mb-5 flex items-center gap-3">
+          <Image src={Pigeon} alt="my gif" height={50} width={50} />
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+        </div>
+
         <p className="font-normal text-gray-700 dark:text-white">
           {description}
         </p>
+        <span className="bg-green-500 bg-orange-500 bg-red-500" />
       </Link>
     </>
   );
